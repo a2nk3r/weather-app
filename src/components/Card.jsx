b4eb5d6c0326aca31ee6c6285f376c03d81073e3
vil-c <amn9955@gmail.com>
2019-01-07 13:34:05 +0530

    static version ready

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 class Card extends Component {
+  styles = {
+    width: 100
+  };
+
   render() {
-    const { day, category, high, low } = this.props.card;
+    const { day, category, high, low, status } = this.props.card;
     return (
-      <div className="weather-card m-3">
+      <div
+        style={this.styles}
+        className="weather-card m-2 border border-success"
+      >
         <span className="badge m-2 badge-primary">{day}</span>
         <div className="category">
-          <img src={category} />
+          <img src={category} alt={status} title={status} />
         </div>
         <div className="temperatures">
           <span className="badge m-2 badge-light">{high}°</span>
---
+++ src/components/Cards.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Card from "./Card";
+class Cards extends Component {
+  render() {
+    const { cards } = this.props;
+    return (
+      <div className="cards">
+        {cards.map(card => (
+          <Card key={card.id.toString()} card={card} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Cards;
